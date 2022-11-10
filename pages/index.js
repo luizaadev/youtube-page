c1945c476aa529c60c0b8d2933feb5112dc1a5e0
luizaadev <luiza_dealmeida@hotmail.com>
2022-11-10 15:09:09 -0300

    Adicionando formulário

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,8 @@ const StyledHeader = styled.div`
 const StyledBanner = styled.div`
     background-color: blue;
     background-image: url(https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80);
-    height: 230px;
+    background-size: cover;
+    height: 230px;    
 `;
 function Header() {
     return (
